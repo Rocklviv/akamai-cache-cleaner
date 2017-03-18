5e82f2ee04d3290e8588a6661e3a4de8f4a90bf2
Denys Chekirda <denys.chekirda@symphony-solutions.eu>
2017-03-19 00:40:51 +0100

    Implemented HTTP Request to akamai with objects to purge.

--- cleaner.go
+++ cleaner.go
@@ -5,9 +5,13 @@ import (
 	"os/user"
 	"fmt"
 	"encoding/json"
+	"encoding/base64"
 	"flag"
 	"path/filepath"
 	"strings"
+	"net/http"
+	"bytes"
+	"io/ioutil"
 )
 
 var (
@@ -29,7 +33,22 @@ type Configuration struct {
 	QueueLengthUri string `json:"queue_length_uri"`
 }
 
-func readConfig() {
+
+type Object struct {
+	Objects []string `json:"objects"`
+}
+
+type CacheResponse struct {
+	EstimatedSeconds 	json.Number	`json:"estimatedSeconds"`
+	ProgressUri 		string		`json:"progressUri"`
+	PurgeId			string		`json:"purgeId"`
+	SupportId		string		`json:"supportId"`
+	HttpStatus		json.Number	`json:"httpStatus"`
+	Detail			string		`json:"detail"`
+	PingAfterSeconds	json.Number	`json:"pingAfterSeconds"`
+}
+
+func readConfig() Configuration {
 	usr, er := user.Current()
 	if er != nil {
 		fmt.Println("Cannot get current user: ", er)
@@ -45,6 +64,7 @@ func readConfig() {
 	if err != nil {
 		fmt.Println("Error reading config file: ", err)
 	}
+	return config
 }
 
 //
@@ -54,7 +74,7 @@ func readConfig() {
 // Returns array
 //
 func getListOfFiles(ext string) []string {
-	fmt.Println("Extension: ", ext)
+	// List of files that will be found by ext.
 	var listFiles []string
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -65,7 +85,7 @@ func getListOfFiles(ext string) []string {
 			httpServers := strings.Split(serverName, ",")
 			newpath := strings.Replace(path, dir, "", -1)
 			for _, a := range extensions {
-				if "."+a == filepath.Ext(info.Name()) {
+				if "." + a == filepath.Ext(info.Name()) {
 					for _, b := range httpServers {
 						httpPath := b + "/" + newpath
 						listFiles = append(listFiles, httpPath)
@@ -78,8 +98,57 @@ func getListOfFiles(ext string) []string {
 	return listFiles
 }
 
+//
+// Returns version of application.
+//
 func returnVersion() {
 	fmt.Println(VERSION)
+}
+
+func sendRequest() {
+	cfg := readConfig()
+	fmt.Println("Preparing request to clear akamai for: ", serverName)
+	files := getListOfFiles(extension)
+	object := Object{Objects: files}
+	encoded, err := json.Marshal(object)
+	if err != nil {
+		fmt.Println("Cannot marshalize value: ", err)
+	}
+	b := bytes.NewBuffer(encoded)
+	fmt.Println("Prepared request: ", b)
+	if err != nil {
+		fmt.Println("Problem with encoding struct. ERROR: ", err)
+	}
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", cfg.APIUrl, b)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "text/plain")
+	req.Header.Add("Authorization", "Basic " + fmt.Sprintf(basicAuth(cfg.Username, cfg.Password)))
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Cannot get response or request is not valid.")
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Cannot read response: ", err)
+	}
+	checkPurge(string(body))
+}
+
+func checkPurge(response string) {
+	var data CacheResponse
+	err := json.Unmarshal([]byte(response), &data)
+	if err != nil {
+		fmt.Println("Cannot unmarshal data: ", err)
+	}
+	fmt.Println(data.EstimatedSeconds)
+}
+
+func basicAuth(username string, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
 
 func main() {
@@ -103,10 +172,7 @@ func main() {
 
 	if domain != nil {
 		serverName = *domain
-		fmt.Println(serverName)
 	}
 
-	readConfig()
-	files := getListOfFiles(*extensionList)
-	fmt.Println("List of files: ", files)
+	sendRequest()
 }
